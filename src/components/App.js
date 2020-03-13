dcf107dce344a4a671262d9a2b3cd42ebcf4979a
Kim <kdw0439@naver.com>
2020-03-13 22:58:31 +0900

    React Text File Upload & Move to Detail page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import AppShell from "./AppShell";
 import Home from "./Home";
 import Texts from "./Texts";
 import Words from "./Words";
+import Detail from "./Detail";
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/texts" component={Texts} />
                     <Route exact path="/words" component={Words} />
+                    <Route exact path="/detail/:textID" component={Detail}/>
                 </div>
             </AppShell>
         </Router>
